750bda2302edfdc7624011320ef2f181420fa577
LiuYuan <ly405653510@gmail.com>
2019-07-22 11:26:49 +0800

    commit

--- deploy/jenkins_build.sh
+++ deploy/jenkins_build.sh
@@ -2,15 +2,15 @@
 set -e
 
 #项目名称
-projectName="cbb_web_service_skeleton"
+projectName="cbb_nb_smart_lock_backend"
 #组件名称
-component_name="业务组件脚手架"
+component_name="智能锁联动业务组件后端服务"
 #组件描述
-component_description="业务组件脚手架"
+component_description="智能锁联动业务组件后端服务"
 #组件标签
 component_tag=${projectName}
 #业务组件端口
-Web_Service_Skeleton_Port=46000
+Web_Service_Skeleton_Port=47000
 
 source ${WORKSPACE}/env/env.sh
 
