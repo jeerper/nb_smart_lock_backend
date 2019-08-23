cde35902d4c55f080a6e07e9a7934d1f9ef2e748
summit_cloud2018/wangyx <sm2019>
2019-08-23 14:59:29 +0800

    commit

--- src/main/java/com/summit/entity/UpdateAlarmParam.java
+++ src/main/java/com/summit/entity/UpdateAlarmParam.java
@@ -1,11 +1,13 @@
 package com.summit.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 @ApiModel(value="锁实时信息类", description="封装所需要的实时信息")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UpdateAlarmParam {
 
     @ApiModelProperty(value="告警id，和accCtrlProId不能同时为空",name="alarmId")
