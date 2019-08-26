b20a3f8b3e0ba47b7b9c7f07bd266903d6739ffe
LiuYuan <ly405653510@gmail.com>
2019-08-26 22:27:54 +0800

    websocket备选方案

--- src/main/java/com/summit/MainAction.java
+++ src/main/java/com/summit/MainAction.java
@@ -1,10 +1,12 @@
 package com.summit;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
@@ -13,9 +15,22 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableEurekaClient
 @EnableFeignClients
 @MapperScan("com.summit.dao")
+//@EnableScheduling
 public class MainAction {
+
+    @Autowired
+    SimpMessagingTemplate messagingTemplate;
     public static final String SnapshotFileName="snapshot";
     public static void main(String[] args) {
         SpringApplication.run(MainAction.class, args);
+    }
+
+//    @Scheduled(fixedDelay = 1000L)
+    public void time() {
+        messagingTemplate.convertAndSend("/topic/realTimeInfoList", "haha");
+    }
+//    @MessageMapping("/realTimeInfo")
+    public void realTimeInfo(String message){
+        System.out.println(message);
     }
 }
