ad31e83ec024e4512dfc46080fb5af529bfeb5ef
qianyunyang <2997272453@qq.com>
2019-08-26 09:06:20 +0800

    commit

---
+++ src/main/java/com/summit/dao/repository/FaceInfoManagerDao.java
@@ -0,0 +1,11 @@
+package com.summit.dao.repository;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.summit.dao.entity.FaceInfo;
+
+/**
+ * Created by Administrator on 2019/8/21.
+ */
+public interface FaceInfoManagerDao extends BaseMapper<FaceInfo> {
+    int insertFaceInfo(FaceInfo faceInfo);
+}
