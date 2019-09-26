e150e19b2aff74f315f24e1985df80daa6a25f2b
qianyy <2997272453@qqcom>
2019-09-26 11:36:16 +0800

    修改人脸过期检查定时时长

--- src/main/java/com/summit/controller/FaceInfoManagerController.java
+++ src/main/java/com/summit/controller/FaceInfoManagerController.java
@@ -357,7 +357,7 @@ public class FaceInfoManagerController {
            }
          }
         return ResultBuilder.buildError(ResponseCodeEnum.CODE_0000,"更新人脸信息全部成功",null);
-      }else if(nowDate<oldfaceEndDate){//不是过期人脸的修改，则需要自己数据库和摄像头同时修改
+      }else if(nowDate<=oldfaceEndDate){//不是过期人脸的修改，则需要自己数据库和摄像头同时修改
         for(String cameraIp:cameraIps){
           DeviceInfo deviceInfo = HuaWeiSdkApi.DEVICE_MAP.get(cameraIp);
           NativeLong ulIdentifyId;
