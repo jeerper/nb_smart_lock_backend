a04321dd635f55190d598dc0dac62162c07f9d09
liuyuan <405653510@qq.com>
2019-10-06 16:51:15 +0800

    查询统计时取消设备电量查询

--- src/main/java/com/summit/controller/AddAccCtrlprocessController.java
+++ src/main/java/com/summit/controller/AddAccCtrlprocessController.java
@@ -4,10 +4,7 @@ import com.summit.common.entity.ResponseCodeEnum;
 import com.summit.common.entity.RestfulEntityBySummit;
 import com.summit.common.util.ResultBuilder;
 import com.summit.dao.entity.AddAccCtrlprocess;
-import com.summit.entity.BackLockInfo;
-import com.summit.entity.LockRequest;
 import com.summit.service.AddAccCtrlprocessService;
-import com.summit.util.AccCtrlProcessUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -28,28 +25,13 @@ import java.util.List;
 public class AddAccCtrlprocessController {
     @Autowired
     private AddAccCtrlprocessService addAccCtrlprocessService;
-    @Autowired
-    private AccCtrlProcessUtil accCtrlProcessUtil;
+
     @ApiOperation(value = "查询全部统计分析信息，其中包括统计分析信息的id,门禁的id,门禁的名称,开锁或者关锁的次数,电池的电量")
     @GetMapping(value = "/selectAddAccCtrlprocess")
     public RestfulEntityBySummit<List<AddAccCtrlprocess>> selectAddAccCtrlprocess() {
         List<AddAccCtrlprocess> addAccCtrlprocesses=null;
         try {
             addAccCtrlprocesses=addAccCtrlprocessService.selectAddAccCtrlprocess(null);
-            LockRequest lockRequest = new LockRequest();
-            for (AddAccCtrlprocess addAccCtrlprocess:addAccCtrlprocesses){
-                    String lockCode = addAccCtrlprocess.getLockCode();
-                    lockRequest.setTerminalNum(lockCode);
-                    BackLockInfo backLockInfo= accCtrlProcessUtil.getLockStatus(lockRequest);
-                    if (backLockInfo == null) {
-                        continue;
-                    }
-                    Integer vol = backLockInfo.getVol();
-                    if (vol == null) {
-                        continue;
-                    }
-                    addAccCtrlprocess.setBatteryLeve(vol);
-            }
         } catch (Exception e) {
             e.printStackTrace();
             log.error("查询全部统计分析信息失败");
