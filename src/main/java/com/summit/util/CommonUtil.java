a49e9c6a31cbaad34340c22863c0d66e8d8733d3
LiuYuan <ly405653510@gmail.com>
2019-10-12 11:13:34 +0800

    文件流finally关闭

--- src/main/java/com/summit/util/CommonUtil.java
+++ src/main/java/com/summit/util/CommonUtil.java
@@ -3,41 +3,49 @@ package com.summit.util;
 import com.summit.constants.CommonConstants;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 
 @Slf4j
 public class CommonUtil {
 
     public static ThreadLocal<DateFormat> timeFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat(CommonConstants.timeFormat));
     public static ThreadLocal<DateFormat> dateFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat(CommonConstants.dateFormat));
-    public static ThreadLocal<DateFormat> snapshotTimeFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat(CommonConstants.snapshotTimeFormat));
+    public static ThreadLocal<DateFormat> snapshotTimeFormat =
+            ThreadLocal.withInitial(() -> new SimpleDateFormat(CommonConstants.snapshotTimeFormat));
     public static ThreadLocal<DateFormat> frontTimeFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat(CommonConstants.frontTimeFormat));
 
     /**
      * string类型时间转换为Date
+     *
      * @param time 待转换string类型时间
      * @param mark 开始或结束时间标志，用于打印日志
      * @return Date类型的时间
      */
-    public static Date strToDate(String time, String mark){
+    public static Date strToDate(String time, String mark) {
 
-        if(time != null && !"".equals(time)){
+        if (time != null && !"".equals(time)) {
             try {
                 return timeFormat.get().parse(time);
             } catch (ParseException e) {
                 String msg = "";
-                if(CommonConstants.STARTTIMEMARK.equalsIgnoreCase(mark)){
+                if (CommonConstants.STARTTIMEMARK.equalsIgnoreCase(mark)) {
                     msg = "开始";
-                }else if(CommonConstants.ENDTIMEMARK.equalsIgnoreCase(mark)){
+                } else if (CommonConstants.ENDTIMEMARK.equalsIgnoreCase(mark)) {
                     msg = "结束";
                 }
-                log.warn(msg+ "时间不合法");
+                log.warn(msg + "时间不合法");
             }
         }
         return null;
@@ -45,12 +53,13 @@ public class CommonUtil {
 
     /**
      * string类型时间转换为Date
-     * @param time 待转换string类型时间
+     *
+     * @param time    待转换string类型时间
      * @param pattern 格式
      * @return Date类型日期
      * @throws ParseException 日期转换异常
      */
-    public static Date parseStr2Date(String time,String pattern) throws ParseException {
+    public static Date parseStr2Date(String time, String pattern) throws ParseException {
         if (CommonUtil.isEmptyStr(time) || CommonUtil.isEmptyStr(pattern))
             return null;
         return new SimpleDateFormat(pattern).parse(time);
@@ -58,11 +67,12 @@ public class CommonUtil {
 
     /**
      * Date类型时间格式化为string
-     * @param time 待转换string类型时间
+     *
+     * @param time    待转换string类型时间
      * @param pattern 格式
      * @return String类型日期
      */
-    public static String formmatDate2Str(Date time,String pattern) {
+    public static String formmatDate2Str(Date time, String pattern) {
         if (time == null || CommonUtil.isEmptyStr(pattern))
             return null;
         return new SimpleDateFormat(pattern).format(time);
@@ -70,6 +80,7 @@ public class CommonUtil {
 
     /**
      * List集合去重
+     *
      * @param list 待去重List集合
      */
     public static void removeDuplicate(List<String> list) {
@@ -80,6 +91,7 @@ public class CommonUtil {
 
     /**
      * List集合判空
+     *
      * @param list 待判空List集合
      * @return 是否为空
      */
@@ -89,6 +101,7 @@ public class CommonUtil {
 
     /**
      * 数组判空
+     *
      * @param arr 待判空数组
      * @return 数组是否为空
      */
@@ -98,6 +111,7 @@ public class CommonUtil {
 
     /**
      * 字符串判空
+     *
      * @param str 待判空字符串
      * @return 字符串是否为空
      */
@@ -106,54 +120,54 @@ public class CommonUtil {
     }
 
     /**
-     *
      * @param beginTime 开始时间
-     * @param endTime 截至时间
+     * @param endTime   截至时间
      * @return true 或 false
      * @throws ParseException
      */
-    public static boolean isInTime(Date beginTime,Date endTime) throws ParseException {
+    public static boolean isInTime(Date beginTime, Date endTime) throws ParseException {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");//设置日期格式
-        Date date=new Date();
+        Date date = new Date();
         String nowtime = df.format(date);
         long nowtime1 = df.parse(nowtime).getTime();
         long biegtime = beginTime.getTime();
         long endtimeTime = endTime.getTime();
-        if (nowtime1>=biegtime && nowtime1<=endtimeTime){
+        if (nowtime1 >= biegtime && nowtime1 <= endtimeTime) {
             return true;
         }
         return false;
     }
 
     /**
-     *  读取文件的流
+     * 读取文件的流
+     *
      * @param path
      * @return 文件内容
      */
-    public static String readFile(String path){
-        BufferedReader  breader=null;
+    public static String readFile(String path) {
+        BufferedReader breader = null;
         StringBuffer sbf = new StringBuffer();
-        FileInputStream fileInputStream=null;
-        InputStreamReader inputStreamReader =null;
+        FileInputStream fileInputStream = null;
+        InputStreamReader inputStreamReader = null;
         try {
             fileInputStream = new FileInputStream(path);
             inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
-            breader=new BufferedReader(inputStreamReader);
+            breader = new BufferedReader(inputStreamReader);
             String tempString;
-            while ((tempString = breader.readLine()) != null){
+            while ((tempString = breader.readLine()) != null) {
                 sbf.append(tempString);
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
-                if (breader !=null){
+                if (breader != null) {
                     breader.close();
                 }
-                if (inputStreamReader !=null){
+                if (inputStreamReader != null) {
                     inputStreamReader.close();
                 }
-                if (fileInputStream !=null){
+                if (fileInputStream != null) {
                     fileInputStream.close();
                 }
             } catch (IOException e) {
@@ -164,36 +178,45 @@ public class CommonUtil {
     }
 
     /**
-     *  把图片转换成base64
-     * @param path  图片路径
-     * @return  图片的base64
+     * 把图片转换成base64
+     *
+     * @param path 图片路径
+     * @return 图片的base64
      * @throws IOException
      */
-    public  static String  imageToBase64Str(String path) throws IOException {
+    public static String imageToBase64Str(String path) throws IOException {
         FileInputStream fis = new FileInputStream(new File(path));
-        byte[] read = new byte[1024];
-        int len = 0;
-        List<byte[]> blist=new ArrayList<byte[]>();
-        int ttllen=0;
-        while((len = fis.read(read))!= -1){
-            byte[] dst=new byte[len];
-            System.arraycopy(read, 0, dst, 0, len);
-            ttllen+=len;
-            blist.add(dst);
-        }
-        fis.close();
-        byte[] dstByte=new byte[ttllen];
-        int pos=0;
-        for (int i=0;i<blist.size();i++){
-            if (i==0){
-                pos=0;
+        String baseStr = null;
+        try {
+            byte[] read = new byte[1024];
+            int len = 0;
+            List<byte[]> blist = new ArrayList<byte[]>();
+            int ttllen = 0;
+            while ((len = fis.read(read)) != -1) {
+                byte[] dst = new byte[len];
+                System.arraycopy(read, 0, dst, 0, len);
+                ttllen += len;
+                blist.add(dst);
             }
-            else{
-                pos+=blist.get(i-1).length;
+
+            byte[] dstByte = new byte[ttllen];
+            int pos = 0;
+            for (int i = 0; i < blist.size(); i++) {
+                if (i == 0) {
+                    pos = 0;
+                } else {
+                    pos += blist.get(i - 1).length;
+                }
+                System.arraycopy(blist.get(i), 0, dstByte, pos, blist.get(i).length);
             }
-            System.arraycopy(blist.get(i), 0, dstByte, pos, blist.get(i).length);
+            baseStr = Base64.getEncoder().encodeToString(dstByte);
+            return baseStr;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            fis.close();
         }
-        String baseStr= Base64.getEncoder().encodeToString(dstByte);
-        return  baseStr;
+        return baseStr;
     }
 }
