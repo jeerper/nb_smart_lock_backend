a38592a4b06dec49e95ee40deca75ebf0098f515
qianyy <2997272453@qqcom>
2019-10-29 16:53:27 +0800

    修改生日字段时间差bug

--- src/main/java/com/summit/dao/entity/FaceInfo.java
+++ src/main/java/com/summit/dao/entity/FaceInfo.java
@@ -21,107 +21,107 @@ import java.util.Date;
  */
 @Data
 @AllArgsConstructor//生成全参的构造函数
-@ApiModel(value = "人脸信息类",description = "封装人脸信息")
-@JsonIgnoreProperties(value = {"facepanorama","facepic","facematch"})
+@ApiModel(value = "人脸信息类", description = "封装人脸信息")
+@JsonIgnoreProperties(value = {"facepanorama", "facepic", "facematch"})
 @TableName(value = "face_info")
 public class FaceInfo {
 
-    @ApiModelProperty(value = "人脸id",name = "faceid")
-    @TableId(value = "face_id",type = IdType.ID_WORKER_STR)
+    @ApiModelProperty(value = "人脸id", name = "faceid")
+    @TableId(value = "face_id", type = IdType.ID_WORKER_STR)
     private String faceid;
 
-    @ApiModelProperty(value = "人脸名",name = "faceName")
+    @ApiModelProperty(value = "人脸名", name = "faceName")
     @TableField(value = "face_name")
     private String faceName;
 
-    @ApiModelProperty(value = "姓名",name = "userName")
+    @ApiModelProperty(value = "姓名", name = "userName")
     @TableField(value = "user_name")
     private String userName;
 
-    @ApiModelProperty(value = "用户id",name = "userID")
+    @ApiModelProperty(value = "用户id", name = "userID")
     @TableField(value = "user_id")
     private String userID;
 
-    @ApiModelProperty(value = "性别",name = "gender")
+    @ApiModelProperty(value = "性别", name = "gender")
     @TableField(value = "gender")
     private Integer gender;
 
-    @ApiModelProperty(value = "生日",name = "birthday")
+    @ApiModelProperty(value = "生日", name = "birthday")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @JsonFormat(pattern="yyyy-MM-dd", timezone="GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     @TableField(value = "birthday")
     private Date birthday;
 
-    @ApiModelProperty(value = "省份",name = "province")
+    @ApiModelProperty(value = "省份", name = "province")
     @TableField(value = "province")
-    private  String province;
+    private String province;
 
-    @ApiModelProperty(value = "城市",name = "city")
+    @ApiModelProperty(value = "城市", name = "city")
     @TableField(value = "city")
     private String city;
 
-    @ApiModelProperty(value = "证件类型",name = "cardType")
+    @ApiModelProperty(value = "证件类型", name = "cardType")
     @TableField(value = "card_type")
     private Integer cardType;
 
-    @ApiModelProperty(value = "证件号",name = "cardID")
+    @ApiModelProperty(value = "证件号", name = "cardID")
     @TableField(value = "card_id")
     private String cardId;
 
-    @ApiModelProperty(value = "匹配率",name = "faceMatchrate")
+    @ApiModelProperty(value = "匹配率", name = "faceMatchrate")
     @TableField(value = "face_match_rate")
     private float faceMatchrate;
 
-    @ApiModelProperty(value = "人脸库名称",name ="facelibname" )
+    @ApiModelProperty(value = "人脸库名称", name = "facelibname")
     @TableField(value = "face_lib_name")
     private String facelibname;
 
-    @ApiModelProperty(value = "人脸库类型",name = "facelibtype")
+    @ApiModelProperty(value = "人脸库类型", name = "facelibtype")
     @TableField(value = "face_lib_type")
     private Integer facelibtype;
 
-   /* @ApiModelProperty(value = "人脸全景图",name = "facepanorama",hidden = true)
-    private FileInfo facepanorama=new FileInfo() ;
+    /* @ApiModelProperty(value = "人脸全景图",name = "facepanorama",hidden = true)
+     private FileInfo facepanorama=new FileInfo() ;
 
-    @ApiModelProperty(value = "人脸识别扣图",name = "facepic",hidden = true)
-    private FileInfo facepic = new FileInfo();
+     @ApiModelProperty(value = "人脸识别扣图",name = "facepic",hidden = true)
+     private FileInfo facepic = new FileInfo();
 
-    @ApiModelProperty(value = "人脸识别和人脸库中匹配的图片",name = "facematch",hidden = true)
-    private FileInfo facematch=new FileInfo();
-*/
-    @ApiModelProperty(value = "摄像头ip地址",name ="deviceip")
+     @ApiModelProperty(value = "人脸识别和人脸库中匹配的图片",name = "facematch",hidden = true)
+     private FileInfo facematch=new FileInfo();
+ */
+    @ApiModelProperty(value = "摄像头ip地址", name = "deviceip")
     @TableField(value = "device_ip")
     private String deviceip;
 
-    @ApiModelProperty(value = "抓拍时间",name = "picsnapshottime")
+    @ApiModelProperty(value = "抓拍时间", name = "picsnapshottime")
     @TableField(value = "pic_snapshot_time")
     private String picsnapshottime;
 
-    @ApiModelProperty(value = "人脸库id",name = "facelibid")
+    @ApiModelProperty(value = "人脸库id", name = "facelibid")
     @TableField(value = "face_lib_id")
     private String facelibid;
 
-    @ApiModelProperty(value = "人脸库图片",name = "faceImage")
+    @ApiModelProperty(value = "人脸库图片", name = "faceImage")
     @TableField(value = "face_image")
     private String faceImage;
 
-    @ApiModelProperty(value = "人脸库类型",name = "faceType")
+    @ApiModelProperty(value = "人脸库类型", name = "faceType")
     @TableField(value = "face_type")
     private Integer faceType;
 
-    @ApiModelProperty(value = "当前人脸录入开始时间",name = "faceStartTime")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="GMT+8")
+    @ApiModelProperty(value = "当前人脸录入开始时间", name = "faceStartTime")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @TableField(value = "face_startTime")
     private Date faceStartTime;
 
-    @ApiModelProperty(value = "当前人脸有限截至时间",name = "faceEndTime")
+    @ApiModelProperty(value = "当前人脸有限截至时间", name = "faceEndTime")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @JsonFormat(pattern="yyyy-MM-dd", timezone="GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     @TableField(value = "face_endTime")
     private Date faceEndTime;
 
-    @ApiModelProperty(value = "人脸是否过期",name = "isValidTime")
+    @ApiModelProperty(value = "人脸是否过期", name = "isValidTime")
     @TableField(value = "is_valid_time")
     private Integer isValidTime;
 
